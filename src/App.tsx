9f18dd95ea5345bafb19c3cfc26f1af5d25cbed1
chenwenhao1108 <wenwen69607@gmail.com>
2024-08-02 18:05:30 +0800

    implement show next card or recite again

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,45 @@ import './App.css'
 import Card from './components/Card'
 
 function App() {
+  const [cards, setCards] = useState([
+    {
+      question: "What is your name?",
+      answer: "Jane Doe"
+    }, {
+      question: "One Plus One?",
+      answer: "2"
+    }
+  ])
+  const [cardsCount, setCardsCount] = useState(0)
+  const [allCardsDone, setAllCardsDone] = useState(false);
+
+  function nextCard()
+  {
+    if(cardsCount === cards.length - 1){
+      setAllCardsDone(true)
+    } else {
+      setCardsCount(cardsCount + 1)
+    }
+  }
+
+  function recite()
+  {
+    setCardsCount(0)
+    setAllCardsDone(false)
+  }
+
+  const cardElemnet = <Card 
+                        key={cardsCount} 
+                        question={cards[cardsCount].question} 
+                        answer={cards[cardsCount].answer}
+                        nextCard={nextCard}
+                        allCardsDone={allCardsDone}
+                        recite={() => recite()}
+                      />
 
   return (
     <>
-      <Card question="What is your name?" answer="Jane Doe"/>
+      {cardElemnet}
     </>
   )
 }
