ba26f0cc1d5eed27d2e87422a8ae2848e455cfa5
chenwenhao1108 <wenwen69607@gmail.com>
2024-08-02 15:35:08 +0800

    display crad

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+
+  return (
+    <>
+      <Card question="What is your name?" answer="Jane Doe"/>
+    </>
+  )
+}
+
+export default App
